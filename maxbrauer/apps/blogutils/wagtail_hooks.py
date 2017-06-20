6c6bf26ee1e3e71e9fbb3456155d2387057f7dfb
Max Brauer <max@transcode.de>
2017-06-20 22:43:49 +0200

    Removed unused import.

--- maxbrauer/apps/blogutils/wagtail_hooks.py
+++ maxbrauer/apps/blogutils/wagtail_hooks.py
@@ -1,5 +1,4 @@
 from django.utils.html import format_html, format_html_join
-from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.conf import settings
 from wagtail.wagtailcore import hooks
 from wagtail.wagtailcore.whitelist import allow_without_attributes, attribute_rule
